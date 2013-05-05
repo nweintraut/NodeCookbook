9e6a14ec472d709e58ea3120037d7bd089fb0d55
Neil Weintraut <neil.weintraut@gmail.com>
2013-05-05 17:19:39 -0400

    Using connect including stacking filters.

--- ch2server.js
+++ ch2server.js
@@ -1,33 +1,19 @@
-var http    = require('http');
+// var http    = require('http');
 var querystring = require('querystring');
 var util = require('util');
+var connect = require('connect');
 var port = process.env.PORT || 3000;
 var form = require('fs').readFileSync('./content/form.html');
 var maxData = 2 * 1024 * 1024; // 2mb
 
-http.createServer(function(request, response){
+// http.createServer(function(request, response){
+connect(connect.limit('64kb'), connect.bodyParser(), function(request, response){
     if(request.method === 'GET') {
         response.writeHead(200, {'Content-Type': 'text/html'});
         response.end(form);
     } else if(request.method === 'POST') {
-      var postData = '';
-      request.on('data', function(chunk){
-          postData += chunk;
-          if (postData.length > maxData) {
-              postData = '';
-              this.pause();
-              response.writeHead(413); // Request entity too large
-              response.end('Too large');
-          }
-      }).on('end', function() {
-          if (!postData) { // prevents empty post requests from crashing the server.
-              response.end(); 
-              return;
-          }
-          var postDataObject = querystring.parse(postData);
-          console.log('User posted: \n' + postDataObject + "\n---");
-          response.end("You posted: \n" + util.inspect(postDataObject) + "\n---");
-      });
+          console.log('User posted: \n' + request.body + "\n---");
+          response.end("You posted: \n" + util.inspect(request.body) + "\n---");
     }
 }).listen(port, function(){
     console.log ("Listening on port " + port + ".");
