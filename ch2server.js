923613b94a0627ab06c35730d9d93fa44337f481
Neil Weintraut <neil.weintraut@gmail.com>
2013-05-05 17:11:46 -0400

    Page 1059. Put limit on size of a POST.

--- ch2server.js
+++ ch2server.js
@@ -3,6 +3,7 @@ var querystring = require('querystring');
 var util = require('util');
 var port = process.env.PORT || 3000;
 var form = require('fs').readFileSync('./content/form.html');
+var maxData = 2 * 1024 * 1024; // 2mb
 
 http.createServer(function(request, response){
     if(request.method === 'GET') {
@@ -12,7 +13,17 @@ http.createServer(function(request, response){
       var postData = '';
       request.on('data', function(chunk){
           postData += chunk;
+          if (postData.length > maxData) {
+              postData = '';
+              this.pause();
+              response.writeHead(413); // Request entity too large
+              response.end('Too large');
+          }
       }).on('end', function() {
+          if (!postData) { // prevents empty post requests from crashing the server.
+              response.end(); 
+              return;
+          }
           var postDataObject = querystring.parse(postData);
           console.log('User posted: \n' + postDataObject + "\n---");
           response.end("You posted: \n" + util.inspect(postDataObject) + "\n---");
