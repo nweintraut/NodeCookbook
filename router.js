abc529ba0ac218c05ff285abfb2a7053191a2915
Neil Weintraut <neil.weintraut@gmail.com>
2013-05-05 14:34:37 -0400

    3rd Example. Cheap multi-level routing. Note that when not using path.basename, the request.url returns a path beginning with a /

--- router.js
+++ router.js
@@ -3,15 +3,18 @@ var path = require("path");
 var port = process.env.PORT || 3000;
 
 var pages = [
-    {route: '', output: 'Woohoo!'},
-    {route: 'about', output: 'A simple routing with Node example'},
-    {route: 'another page', output: function(){return 'Here\'s ' + this.route;}}
+    {route: '/', output: 'Woohoo!'},
+//    {route: 'about', output: 'A simple routing with Node example'},
+    {route: '/about/this', output: "Multilevel routing with Node"},
+    {route: "/about/node", output: "Evented I/O for V8 Javascript"},
+    {route: '/another page', output: function(){return 'Here\'s ' + this.route;}}
 ];
 var server = http.createServer(function(request, response){
-    var lookup = path.basename(decodeURI(request.url));
+    var lookup = decodeURI(request.url);
+    console.log("Lookup is: " + lookup + "]");
+//    var lookup = path.basename(decodeURI(request.url));
     pages.forEach(function(page){
         if(page.route === lookup) {
-            console.log("Passed");
             response.writeHead(200, {'Content-Type': 'text/html'});
             response.end(typeof page.output === 'function' ? page.output(): page.output);
         }
