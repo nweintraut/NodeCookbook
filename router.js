78960100d48093e306b7bffbb503a2e059a3e0b2
Neil Weintraut <neil.weintraut@gmail.com>
2013-05-05 14:42:19 -0400

    Page 579. Demonstration of interpreting query strings in request path. ?id=x.

--- router.js
+++ router.js
@@ -1,24 +1,24 @@
 var http = require('http');
 var path = require("path");
+var url  = require('url');
 var port = process.env.PORT || 3000;
 
 var pages = [
-    {route: '/', output: 'Woohoo!'},
-//    {route: 'about', output: 'A simple routing with Node example'},
-    {route: '/about/this', output: "Multilevel routing with Node"},
-    {route: "/about/node", output: "Evented I/O for V8 Javascript"},
-    {route: '/another page', output: function(){return 'Here\'s ' + this.route;}}
+    {id: '1', route: '', output: 'Woohoo!'},
+    {id: '2', route: 'about', output: "A simple routing with Node example"},
+    {id: '3', route: 'another page', output: function(){return 'Here\'s ' + this.route;}}
 ];
 var server = http.createServer(function(request, response){
-    var lookup = decodeURI(request.url);
-    console.log("Lookup is: " + lookup + "]");
-//    var lookup = path.basename(decodeURI(request.url));
-    pages.forEach(function(page){
-        if(page.route === lookup) {
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.end(typeof page.output === 'function' ? page.output(): page.output);
-        }
-    });
+    var id = url.parse(decodeURI(request.url), true).query.id;
+    console.log("Id is: " + id);
+    if(id) {
+        pages.forEach(function(page){
+            if(page.id === id) {
+                response.writeHead(200, {'Content-Type': 'text/html'});
+                response.end(typeof page.output === 'function' ? page.output(): page.output);
+            }
+        });
+    }
     if(!response.finished){
         response.writeHead(404);
         response.end("Page Not Found");
