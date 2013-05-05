f2d9d784d787e7274f34b9b0fbe982276d7d9fcd
Neil Weintraut <neil.weintraut@gmail.com>
2013-05-05 15:00:21 -0400

    Basic proof of static file identification. Page 613.

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var http = require('http');
 var path = require("path");
+var fs  = require('fs');
 var url  = require('url');
 var port = process.env.PORT || 3000;
 
+var mimeTypes = {
+    '.js' : 'text/javascript',
+    '.html': 'text/html',
+    '.css': 'text/css'
+};
 var pages = [
     {id: '1', route: '', output: 'Woohoo!'},
     {id: '2', route: 'about', output: "A simple routing with Node example"},
@@ -10,6 +16,12 @@ var pages = [
 ];
 var server = http.createServer(function(request, response){
     var id = url.parse(decodeURI(request.url), true).query.id;
+    var lookup = path.basename(decodeURI(request.url)) || 'index.html';
+    var f = 'content/' + lookup;
+    fs.exists(f, function(exists){
+        console.log( exists ? lookup + " is there" : lookup + " doesn't exist");
+    });
+    /*
     console.log("Id is: " + id);
     if(id) {
         pages.forEach(function(page){
@@ -23,6 +35,7 @@ var server = http.createServer(function(request, response){
         response.writeHead(404);
         response.end("Page Not Found");
     }
+    */
 }).listen(port, function(){
     console.log ("Listening on port " + port + ".");
 });
