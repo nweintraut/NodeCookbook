df3b0a3d70ebc95e7f7b2bcb6a906f9a49e030ef
Neil Weintraut <neil.weintraut@gmail.com>
2013-05-05 21:11:51 -0400

    Updated

--- Redis/redis_config.js
+++ Redis/redis_config.js
@@ -1,5 +1,5 @@
 module.exports = {
     redisHost: "slimehead.redistogo.com",
     redisPort: "9285",
-    redisAuthentication: "placeholder"
+    redisAuthentication: "202ce927a499e2238aaaa95b1430d956"
 };
