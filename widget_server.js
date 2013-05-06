fa4b7bc09c22df8d12361f12c1421156c0d897a9
Neil Weintraut <neil.weintraut@gmail.com>
2013-05-06 09:02:16 -0700

    Socket.io widget working with Redis.

--- widget_server.js
+++ widget_server.js
@@ -1,12 +1,44 @@
 var io = require('socket.io').listen(8081);
 var sioclient = require('socket.io-client');
+// var   redis     = require('redis')
+var   redis         = require('socket.io/node_modules/redis')
+    , RedisStore    = require('socket.io/lib/stores/redis')
+    , redisConfig = require('./Redis/redis_config');
+
+
+function makeRedisClient() { 
+  var redisClient = redis.createClient(redisConfig.redisPort, redisConfig.redisHost, {});
+  redisClient.on("error", function(err){
+    console.log("Redis Error: [" + err + "]");
+  });
+  redisClient.auth(redisConfig.redisAuthentication, function(error, reply){
+    if (error) {console.log(">>>>>>>> Redis Authentication Failed: [" + error +"]");}
+    else {console.log("Redis Authentication Response is: [" + reply + "]");}
+  });
+  return redisClient;
+}
+
+var pub = makeRedisClient()
+  , sub = makeRedisClient()
+  , client = makeRedisClient(); 
+
 var widgetScript = require('fs').readFileSync('widget_client.js');
 var url = require('url');
 
 var totals = {};
+totals = client;
 
 io.configure(function(){
    io.set('resource', '/loc');
+   
+   io.set('store', new RedisStore({
+      redis: redis
+    , redisPub: pub
+    , redisSub: sub
+    , redisClient: client
+    })
+   );
+
    io.enable('browser client gzip');
 });
 sioclient.builder(io.transports(), function(err, siojs){
@@ -19,14 +51,18 @@ sioclient.builder(io.transports(), function(err, siojs){
 });
 io.sockets.on('connection', function(socket){
    var origin = (socket.handshake.xdomain) ? url.parse(socket.handshake.headers.origin). hostname : 'local';
-   console.log(origin);
-   totals[origin] = (totals[origin]) || 0;
-   totals[origin] += 1;
+//   totals[origin] = (totals[origin]) || 0;
+//   totals[origin] += 1;
    socket.join(origin);
+   totals.incr(origin, function(err, total){
+    io.sockets.to(origin).emit('total', total);
+   });
    
    io.sockets.to(origin).emit('total', totals[origin]);
    socket.on('disconnect', function(){
-       totals[origin] -= 1;
-       io.sockets.to(origin).emit('total', totals[origin]);
+       totals.decr(origin, function(err, total){
+          io.sockets.to(origin).emit('total', total);
+
+       });
    });
 });
